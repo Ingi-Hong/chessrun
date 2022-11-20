32ac2a74351402808149a49a165e64a5639d7071
Ingi-Hong <ingihong@bu.edu>
2022-11-19 19:20:04 -0500

    Ran npx create-react-app

---
+++ src/Components/Sidebardata.js
@@ -0,0 +1,18 @@
+const Sidebaradatalist = [
+    {
+        name: "Search",
+        function: "Search"
+    }, 
+
+    {
+        name: "Elo Range",
+        function: "EloSlider"
+    }, 
+
+    {
+        name: "Opening",
+        function: "Opening"
+    }
+];
+
+export default Sidebaradatalist;
